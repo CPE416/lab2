4bd01c5c0b058e849f702bd175a8ba719e9f75ad
Spencer Shaw <sfshaw@calpoly.edu>
2019-01-18 17:21:24 -0800

    Move motors to hardware.h

--- hardware.h
+++ hardware.h
@@ -13,4 +13,32 @@ void led_low(u08 num){
 	digital_out(num, 0);
 }
 
+
+void process_data(u08 *data, u08 *vals){
+
+
+    // switch (MODE){
+    //     case MODE_2A:
+    //         break;
+    //     case MODE_2B:
+    //         break;
+    //     case MODE_3A:
+    //         break;
+    //     case MODE_3B:
+    //         break;
+    // }
+    
+    // data[0] = map_motor_value(vals[0]);
+    // data[1] = map_motor_value(vals[1]);
+
+
+    data[0] = vals[0];
+    data[1] = vals[1];
+}
+
+void set_motors(u08 *data){
+    set_servo(MOTOR_1, data[0]);
+    set_servo(MOTOR_2, data[1]);
+}
+
 #endif
--- lab2_part1.c
+++ lab2_part1.c
@@ -8,9 +8,10 @@
 
 // Custum delay lib
 #include "delay.h"
+#include "hardware.h"
 
 // Settings
-#define DELAY_MS 50// Delay time for loop
+#define DELAY_MS 100 // Delay time for loop
 #define MODE MODE_2A // Braitenberg mode
 
 // Pin map
@@ -36,6 +37,7 @@ int main(void)
     init(); // Init board
     init_servo();
 
+
     u08 vals[] = {0, 0}; // Data retrieve from lgith sensors
     u08 data[] = {0, 0}; 
     // Place text at start of buffer
@@ -47,7 +49,8 @@ int main(void)
         read_data(vals);
         process_data(data, vals);
     print_data(vals, data);
-        set_motors(data);
+    if(motors_running){
+        set_motors(data);}
         delay_ms(DELAY_MS);
     }
     return 1;
@@ -57,34 +60,8 @@ void read_data(u08 *vals){
     // vals[0] = analog(LIGHT_SENSOR_1_PIN);
     // vals[1] = analog(LIGHT_SENSOR_2_PIN);
 
-    vals[0] = 255;
-    vals[1] = 0;
-}
-
-void process_data(u08 *data, u08 *vals){
-
-    // switch (MODE){
-    //     case MODE_2A:
-    //         break;
-    //     case MODE_2B:
-    //         break;
-    //     case MODE_3A:
-    //         break;
-    //     case MODE_3B:
-    //         break;
-    // }
-    
-    // data[0] = map_motor_value(vals[0]);
-    // data[1] = map_motor_value(vals[1]);
-
-
-    data[0] = vals[0];
-    data[1] = vals[1];
-}
-
-void set_motors(u08 *data){
-    set_servo(MOTOR_1, data[0]);
-    set_servo(MOTOR_2, data[1]);
+    vals[0] = vals[0] + 1;
+    vals[1] = vals[1] + 1;
 }
 
 void  print_data(u08 *vals, u08 *data){
