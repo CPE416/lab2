ca5e55df4a5f67c5cffd5ab66a2f466141f46c40
Spencer Shaw <sfshaw@calpoly.edu>
2019-01-23 16:59:44 -0800

    Work on part1

--- lab2_part1.c
+++ lab2_part1.c
@@ -2,6 +2,7 @@
 // Lab 2 Part 1
 // Description: 
 
+#include <stdio.h>
 
 // Custum delay lib
 #include "delay.h"
@@ -9,56 +10,32 @@
 
 // Settings
 #define DELAY_MS 200 // Delay time for loop
-#define MODE MODE_2A // Braitenberg mode
-
-
-#define MODE_2A 0
-#define MODE_2B 1
-#define MODE_3A 2
-#define MODE_3B 3
-
-#define TRUE 1
-#define FALSE 0
-
-void print_data(int *vals, u08 *data);
+#define DELTA_INIT 4 // Braitenberg mode
 
 int main(void)
 {
-    init(); // Init board
-    init_servo();
-
-    u08 motors_running = TRUE;
-    int vals[] = {0, 0}; // Data retrieve from lgith sensors
-    u08 data[] = {0, 0}; 
-    // Place text at start of buffer
-    // sprintf(buffer, "416");
-
-    // Main loop
-    while (1)
+    init();
+    motor(0, 0);
+    motor(1, 0);
+    print_string("Hello Jared!");
+    delay_s(4);
+    print_data('a', 'b', 'c', 'd');
+    int speed = 0;
+    int delta = DELTA_INIT;
+    char* buf = "aaaabbbb";
+    while (1)   
     {
-     read_light_sensor(vals);
-        process_data(data, vals);
-    print_data(vals, data);
-    if(motors_running){
-        set_motors(data);}
+        motor(0, speed);
+        motor(1, speed);
+
+        sprintf(buf, "%d", speed);
+        print_data(buf, " ", " ", " ");
         delay_ms(DELAY_MS);
+        speed = speed + delta;
+        if ((speed > 96) || (speed < -96)){
+            // speed = speed - (speed % 100);
+            delta = 0 - delta;
+        }
     }
-    if(get_btn()){
-        motors_running = ! motors_running;
-    }
-    return 1;
-}
-
-
-void  print_data(int *vals, u08 *data){
-    clear_screen();
-    lcd_cursor(0, 0);
-    print_num(vals[0]);
-    lcd_cursor(4, 0);
-    print_num(data[0]);
-    lcd_cursor(0, 1);
-    print_num(vals[1]);
-    lcd_cursor(4, 1);
-    print_num(data[1]);
-    
+    return 27;
 }
